4263b48265dba3a2f2da3517ba44d2a806a0564c
유보미 <bomeeryu_c@yubomiui-MacBook-Pro.local>
2017-06-23 17:35:34 +0900

    아 어렵다 Retrofit2+gson

---
+++ app/src/main/java/org/adroidtown/practiceapp/NetworkInterface.java
@@ -0,0 +1,8 @@
+package org.adroidtown.practiceapp;
+
+/**
+ * Created by bomeeryu_c on 2017. 6. 23..
+ */
+
+public interface NetworkInterface {
+}
---
+++ app/src/main/java/org/adroidtown/practiceapp/WeatherModel.java
@@ -0,0 +1,17 @@
+package org.adroidtown.practiceapp;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by bomeeryu_c on 2017. 6. 23..
+ */
+
+public class WeatherModel {
+
+    @SerializedName("tmin")
+    private String tMin;
+
+    @SerializedName("tMax")
+    private String tMax;
+
+}
